ba3a362e52ad38e349b3485e7108fa9c75db7916
Marcell Kujbus <marcellkujbus@gmail.com>
2020-06-28 11:56:58 +0200

    formatting

--- src/main/scala/exercises/AnotherSEt.scala
+++ src/main/scala/exercises/AnotherSEt.scala
@@ -1,8 +1,9 @@
 package exercises
 
-object AnotherSEt extends App{
- val a = MySet(1,2,3)
- println(a)
- // try jump to definition: leader gd and then ctrl o ctrl i
+object AnotherSEt extends App {
+  val a = MySet(1, 2, 3)
+  println(a)
 
+  def f(name: String) = name + "lol"
 }
+
--- src/test/scala/MyStreamTest.scala
+++ src/test/scala/MyStreamTest.scala
@@ -3,39 +3,41 @@ import org.scalatest.matchers.should.Matchers
 import exercises.MyStream
 import exercises.MyStreamFactory
 import exercises.EmptyStream
+import scala.runtime.LazyInt
 
 class MyStreamTest extends AnyFlatSpec with Matchers {
-  val naturals = MyStream.from(1)(_ + 1)
+
   "The stream of naturals" should "start with 1" in {
+    val naturals = MyStream.from(1)(_ + 1)
     naturals.head shouldBe 1
   }
+
   it should "map correctly" in {
+    val naturals = MyStream.from(1)(_ + 1)
     naturals.map(_ * 2).tail.head shouldBe 4
   }
-  /*
-   [first, [...]
-   [first, fibo(second, first + second)
-   */
-  def fibonacci(first: BigInt, second: BigInt): MyStream[BigInt] =
-    new MyStreamFactory(first, fibonacci(second, first + second))
+
   "The stream of the fibonacci numbers" should "work with big indexes" in {
+    /*
+     [first, [...]
+     [first, fibo(second, first + second)
+     */
+    def fibonacci(first: BigInt, second: BigInt): MyStream[BigInt] =
+      new MyStreamFactory(first, fibonacci(second, first + second))
     val a = fibonacci(1, 1).takeAsList(1000)
     a should have size 1000
   }
 
-  // eratosthenes sieve
-  def eratosthenes(numbers: MyStream[Int]): MyStream[Int] =
-    if (numbers.isEmpty) numbers
-    else
-      new MyStreamFactory(
-        numbers.head,
-        eratosthenes(numbers.tail.filter(n => n % numbers.head != 0))
-      )
-
   "The stream of prime numbers" should "work with big indexes" in {
+    // eratosthenes sieve
+    def eratosthenes(numbers: MyStream[Int]): MyStream[Int] =
+      if (numbers.isEmpty) numbers
+      else
+        new MyStreamFactory(
+          numbers.head,
+          eratosthenes(numbers.tail.filter(n => n % numbers.head != 0))
+        )
     val a = eratosthenes(MyStream.from(2)(_ + 1)).takeAsList(1000)
     a should have size 1000
   }
-
 }
-
