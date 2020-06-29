a6f0eb83562ba49ad7dd69630f808e74986ea855
Marcell Kujbus <marcellkujbus@gmail.com>
2020-06-29 14:06:33 +0200

    instance equality and monad laws testing

--- src/main/scala/exercises/MyLazy.scala
+++ src/main/scala/exercises/MyLazy.scala
@@ -3,6 +3,12 @@ package exercises
 class MyLazy[+A](elem: => A) {
   lazy val inside = elem
   def flatMap[B >: A](f: A => MyLazy[B]): MyLazy[B] = f(elem)
+
+  final override def equals(x: Any): Boolean = {
+    val that = x.asInstanceOf[MyLazy[A]]
+    if (that == null) false
+    else this.inside == that.inside
+  }
 }
 object MyLazy {
   def apply[A](value: => A): MyLazy[A] = {
--- src/test/scala/LazyTest.scala
+++ src/test/scala/LazyTest.scala
@@ -1,7 +1,6 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import exercises.MyLazy
-import org.scalatest.PrivateMethodTester
 class LazyTest extends AnyFlatSpec with Matchers {
 
   "Lazy values" should "be evaluated when needed" in {
@@ -15,5 +14,31 @@ class LazyTest extends AnyFlatSpec with Matchers {
     val cond = false
     val a: Int = if (cond && lazyBool.inside) 10 else 20
     a shouldBe 20
+  }
+
+  "Layy monad" should "obey to right identity law" in {
+    val a = 3
+
+    MyLazy(3).flatMap(x => MyLazy(x)) shouldBe MyLazy(a)
+  }
+
+  it should "obey to left identity law" in {
+    val a = "hello"
+    val f: String => MyLazy[String] = x => MyLazy(x + ", oh hi")
+
+    MyLazy(a).flatMap(f) shouldBe f(a)
+  }
+
+  it should "be associative" in {
+    // like 1 + (2 + 3) = (1 + 2) + 3
+    // this is to make flatMap chainable, as it is a binary operator
+    val a = "hello"
+    val f: String => MyLazy[String] = x => MyLazy(x + ", oh hi")
+    val g: String => MyLazy[String] = x => MyLazy(x + ", whatsup?")
+
+    MyLazy(a).flatMap(f).flatMap(g) shouldBe {
+      MyLazy(a).flatMap(x => f(x).flatMap(g))
+    }
+
   }
 }
